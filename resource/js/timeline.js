ff86b38fe9026372d97bcfef8ba3aa624d0b0df1
hyunjoo2 <ttldlguswn@gmail.com>
2022-02-23 10:43:19 +0900

    전체 색상 변경

--- resource/js/timeline.js
+++ resource/js/timeline.js
@@ -3,6 +3,8 @@ $(function () {
     fnScrollDown();
     buttonClick();
     tabSelect();
+    //clickboardList();
+    fixYear();
 })
 
 function fnTop() {
@@ -14,19 +16,19 @@ function fnTop() {
 
 function fnScrollDown() {
     //스크롤 발생 이벤트 처리
-    // $('#timeline').scroll(function () {
-    //     var scrollT = $(this).scrollTop(); //스크롤바의 상단위치
-    //     var scrollH = $(this).height(); //스크롤바를 갖는 div의 높이
-    //     var contentH = $('.timeline__inner').height(); //문서 전체 내용을 갖는 div의 높이
+    $('#timeline').scroll(function () {
+        var scrollT = $(this).scrollTop(); //스크롤바의 상단위치
+        var scrollH = $(this).height(); //스크롤바를 갖는 div의 높이
+        var contentH = $('.timeline__inner').height(); //문서 전체 내용을 갖는 div의 높이
 
-    //     $(".timeline-button__bottom button").click(function () {
-    //         $(".timeline__inner").scrollTop($(document).height());
-    //     });
+        $(".timeline-button__bottom button").click(function () {
+            $(".timeline__inner").scrollTop($(document).height());
+        });
 
-    //     // if(scrollT + scrollH +1 >= contentH) { // 스크롤바가 아래 쪽에 위치할 때
-    //     //     $('.timeline__inner').append(imgs);
-    //     // }
-    // });
+        // if(scrollT + scrollH +1 >= contentH) { // 스크롤바가 아래 쪽에 위치할 때
+        //     $('.timeline__inner').append(imgs);
+        // }
+    });
 }
 
 function buttonClick() {
@@ -34,9 +36,9 @@ function buttonClick() {
     $(".timeline-item .button-wrap button").click(function () {
         $(".content_wrap").addClass("step2");
         $(".content-left").addClass("step2");
-        $(".content-right").show();
+        $(".content-right").removeClass("step1");
         $(this).parents().find('.timeline-content').children().find(".timeline-item").removeClass("on");
-        $(this).parents().closest('.timeline-item').addClass("on");
+        $(this).parents().closest('.timeline-content').children().addClass("on");
 
 
         //
@@ -90,7 +92,7 @@ function buttonClick() {
     $(".btnRightSwitch button").click(function () {
         $(".content_wrap").removeClass("step2");
         $(".content-left").removeClass("step2");
-        $(".content-right").hide();
+        $(".content-right").addClass("step1");
     });
 }
 
@@ -127,4 +129,21 @@ function tabSelect() {
     });
 
    
+}
+
+
+
+function fixYear(){
+    var filter_Offset = $('.timeline-box .year').offset();
+		$(window).scroll(function() 
+		{
+			if ( $(document).scrollTop() > filter_Offset.top ) 
+			{
+			   $('.timeline-box .year').addClass('fix');
+			} 
+			else 
+			{
+			   $('.timeline-box .year').removeClass('fix');
+			}
+		});
 }
