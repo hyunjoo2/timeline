b019729e5da657678237e0f5f3318ae8d4f2a217
hyunjoo2 <ttldlguswn@gmail.com>
2022-03-03 16:55:17 +0900

    title button 클릭시 tabcontent 활성화 수정중

--- resource/js/timeline_220302.js
+++ resource/js/timeline_220302.js
@@ -267,9 +267,11 @@ function titClick() {
 
 function tabSelect() {
     var $tabButtonItem = $('#tab-button li'),
+        $Button = $('.button-wrap button'),
         $tabTit = $('.timeline-content>div .tit'),
         $tabSelect = $('#tab-select'),
         $tabContents = $('.tab-contents'),
+        
         activeClass = 'active';
 
     $tabButtonItem.first().addClass(activeClass);
@@ -280,12 +282,32 @@ function tabSelect() {
     //     var target = $(this).attr('href');
 
     //     $tabButtonItem.removeClass(activeClass);
-    //     $tabButtonItem.parent().addClass(activeClass);
+    //     $tabButtonItem.first().addClass(activeClass);
+    //     //$('a[href="#tab01"]').parent().addClass(activeClass);
+    //     $('#tab-button').css('transform','translate3d(0px, 0px, 0px)')
+
+    //     //$('#' + target).parant().addClass(activeClass);
+
+    //     $tabButtonItem.removeClass(activeClass);
+    //     $(this).parent().addClass(activeClass);
     //     $tabSelect.val(target);
     //     $tabContents.hide();
     //     $(target).show();
     //     e.preventDefault();
+
+    
     // });
+
+    // button
+    $Button.find('a').on('click', function (e) {
+        var target = $(this).attr('href');
+        $tabButtonItem.removeClass(activeClass);
+        $tabButtonItem.parent().addClass(activeClass);
+        $tabSelect.val(target);
+        $tabContents.hide();
+        $(target).show();
+        e.preventDefault();
+    });
 
     // button
     $tabButtonItem.find('a').on('click', function (e) {
